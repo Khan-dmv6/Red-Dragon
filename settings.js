ad6707df14f6f45a0f9d096c33313baa11c0d47f
Khan-dmv6 <108912344+Khan-dmv6@users.noreply.github.com>
2022-07-10 08:03:20 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is 🇵🇰devil king🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+923187028664'] //Owner number in aive msg
-global.premium = ['+923187028664'] //Owner Number info
-global.ownernomer = '+923187028664' //Owner Number <<<
+global.owner = ['+923080613785'] //Owner number in aive msg
+global.premium = ['+923080613785'] //Owner Number info
+global.ownernomer = '+923080613785' //Owner Number <<<
 
 global.ownername = '😎🇵🇰devil king' //Owner Name
 global.botname = ' ☬🇵🇰devil༒king࿐' //Bot Name
